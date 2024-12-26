f44a72ac75039f99db81ba16264b3d635933de79
JOSE TUÑON <jose.tunon@itxpertix.com>
2024-12-26 12:45:16 -0500

    Se modifica url del backend para el arranque en la nube

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: Number(process.env.PORT) || 3000, // Usa el puerto proporcionado por Railway o 3000 como respaldo
+  },
+  preview: {
+    port: Number(process.env.PORT) || 3000, // Configura el puerto para el servidor de previsualización
+  },
 })
