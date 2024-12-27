b776d8d38b47d89bff618598eb505220f937a2ff
JOSE TUÑON <jose.tunon@itxpertix.com>
2024-12-27 10:45:54 -0500

    Se realizan ajustes en la funcion que maneja puntos de miles

--- src/utils/utils.tsx
+++ src/utils/utils.tsx
@@ -1,6 +1,12 @@
-export const formatPrice = (value: number): string => {
-  const formattedValue = value.toLocaleString('es-ES');
-  return formattedValue;
+export const formatPrice = (value: number): void => {
+  let num = value.toString().replace(/[.,]/g, "");
+  if (!isNaN(Number(num))) {
+    num = num.split("").reverse().join("").replace(/(?=\d*\.?)(\d{3})/g, "$1.");
+    num = num.split("").reverse().join("").replace(/^[\.]/, "");
+    value = Number(num);
+  } else {
+    value = Number(value.toString().replace(/[^\d\.]*/g, ""));
+  }  
 };
 
 export const removeFormatPrice = (value: string): string => {
