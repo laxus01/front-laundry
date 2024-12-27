778099590f286d54a83e29ad20b3734d0809163c
JOSE TUÑON <jose.tunon@itxpertix.com>
2024-12-27 15:09:10 -0500

    Se realizan ajustes en la funcion que maneja puntos de miles

--- src/utils/utils.tsx
+++ src/utils/utils.tsx
@@ -1,13 +1,5 @@
 export const formatPrice = (value: any): string => {
-  const stringValue = value.toString();
-  const length = stringValue.length;
-
-  if (length > 3 && length < 7) {
-    return stringValue.slice(0, length - 3) + '.' + stringValue.slice(length - 3);
-  } else if (length >= 7) {
-    return stringValue.slice(0, length - 6) + '.' + stringValue.slice(length - 6, length - 3) + '.' + stringValue.slice(length - 3);
-  }
-  return stringValue;
+  return new Intl.NumberFormat('es-ES', { style: 'currency', currency: 'EUR' }).format(value);
 };
 
 export const removeFormatPrice = (value: string): string => {
